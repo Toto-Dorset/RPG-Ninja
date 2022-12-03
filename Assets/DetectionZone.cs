a7c64e09d31b952fa13ec376ee9a1a2dcef0d561
cotelvince <26045@student.dorset-college.ie>
2022-12-03 18:10:57 +0100

    enemy detect the player

---
+++ Assets/DetectionZone.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectionZone : MonoBehaviour
+{
+    public string tagPlayer = "Player";
+    public List<Collider2D> detectedObjs = new List<Collider2D>();
+    public Collider2D col;
+
+
+    void OnTriggerEnter2D(Collider2D collider){
+        if(collider.gameObject.tag == tagPlayer){
+            detectedObjs.Add(collider);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider){
+        detectedObjs.Remove(collider);
+    }
+
+}
--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -7,9 +7,26 @@ public class Enemy : MonoBehaviour
 
     Animator animator;
 
+    public DetectionZone detectionZone;
+
+    public float moveSpeed = 500f;
+
+    Rigidbody2D rb;
+
+
     private void Start() {
         animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
+
+     void FixedUpdate() {
+        if(detectionZone.detectedObjs.Count >= 0){
+            Vector2 direction =(Vector2) (detectionZone.detectedObjs[0].transform.position - transform.position).normalized;
+
+            rb.AddForce(direction * moveSpeed * Time.deltaTime);
+        }
+    }
+
     public float Health{
 
         set{health=value;

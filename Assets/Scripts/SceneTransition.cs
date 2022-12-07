0a536453f2f5fdbaa412f4c6690c4f2386c50722
Toto-Dorset <26043@student.dorset-college.ie>
2022-12-07 12:10:17 +0000

    update

---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    public string sceneToLoad;
+
+    /// <summary>
+    /// Sent when another object enters a trigger collider attached to this
+    /// object (2D physics only).
+    /// </summary>
+    /// <param name="other">The other Collider2D involved in this collision.</param>
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player" && !other.isTrigger)
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
